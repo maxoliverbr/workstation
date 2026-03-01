bb897e43cb879742f0d3e2706ba5c85b288d185b
Max Oliver <max.oliver@cintrax.com.br>
2026-03-01 12:17:05 -0300

    New backup

---
+++ b.sh
@@ -0,0 +1,12 @@
+tar --exclude='*/Cache' \
+    --exclude='*/CacheStorage' \
+    --exclude='*/ScriptCache' \
+    --exclude='*/node_modules' \
+    --exclude='*/OptGuideOnDeviceModel' \
+    --exclude='*/GPUCache' \
+    --exclude='*/Code Cache' \
+    --exclude='*/DawnCache' \
+    --exclude='*/Session Storage' \
+    --exclude='*/Service Worker/CacheStorage' \
+    -czvf dev-backup.tar.gz \
+    ~/.ssh ~/.gnupg ~/.config ~/.local/share/gnome-shell/extensions ~/.bashrc ~/.zshrc
